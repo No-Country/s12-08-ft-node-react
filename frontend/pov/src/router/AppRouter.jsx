a639a15092e52426c1861268d6980c2ab0143183
Emanuel Valente <emavalente.dev@gmail.com>
2023-11-21 17:17:00 -0300

    Elimino archivos sobrantes - frontend - 21/11/23

--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "../App";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
