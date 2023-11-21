33600e5b677cc1ac2d125018d6fe0862fd837e44
montoyitadevelp <simon1018226487@gmail.com>
2023-11-21 15:41:56 -0500

    add: Integration examples for customs styles

--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import '../index.css';
 const AppRouter = () => {
   return (
     <>
