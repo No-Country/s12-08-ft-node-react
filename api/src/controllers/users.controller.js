0296d9e7443baf92470f85ea0ce96917f06d4223
josefinaeciolaza <josefinaecio@gmail.com>
2023-11-27 13:32:56 -0300

    Models user and relations

---
+++ api/src/controllers/users.controller.js
@@ -0,0 +1,17 @@
+const { User } = require('../db');
+const validations = require('../validations/users.validations.js');
+
+class UserController {
+  static async example(req,res,next) {
+    console.log('example!');
+    try {
+      await validations.validateAsync(req.body);
+
+      // call db methods after validations have passed
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = { UserController };
