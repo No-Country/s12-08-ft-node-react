fb462a1d16a21bcce3ad67698c3211b3d6a0da97
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-11-27 12:19:26 -0300

    Swagger config
    Swagger config and doc for example

--- api/src/routes/examples.js
+++ api/src/routes/examples.js
@@ -2,6 +2,32 @@ const express = require("express");
 const examplesRouter = express.Router();
 const { ExampleController } = require("../controllers/examples.controller");
 
+
+/**
+ * @openapi
+ * /api/examples:
+ *   get:
+ *     tags:
+ *       - Examples
+ *     summary: Obtiene un ejemplo.
+ *     description: Obtiene un ejemplo.
+ *     responses:
+ *       200:
+ *         description: Ejemplo obtenido con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       500:
+ *         description: Error del servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+*/
 examplesRouter.get("/", ExampleController.example);
 
 module.exports = examplesRouter;
