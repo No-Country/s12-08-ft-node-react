50c40b7bd500541db4e5dcbcdd7e48b797b5a39f
Joseacode <josea_@hotmail.com>
2023-11-28 16:38:47 -0300

    front: registerlogin page

--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from '../pages/Home';
-import '../index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import Registerlogin from "../pages/regiterlogin/regiterlogin";
+import "../index.css";
 
 const AppRouter = () => {
-
-
+  
+  
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Registerlogin />} />
+          <Route path="/login" element={<Registerlogin />} />
+
         </Routes>
       </BrowserRouter>
     </>
--- frontend/pov/src/slices/store/store.js
+++ frontend/pov/src/slices/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "../rootReducer/combineReducers";
+import rootReducer from "../rootReducer/combineReducers.js";
 
 const store = configureStore({
   reducer: rootReducer,
