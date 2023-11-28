5226013a1fbd3423a68e81a08b62698d9621c08a
montoyitadevelp <simon1018226487@gmail.com>
2023-11-28 08:16:00 -0500

    add: Home route

--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "../components/NavBar/NavBar";
 import { Home } from "../pages/Home";
+import NavBar from "../components/NavBar/NavBar";
 import "../index.css";
+
 
 const AppRouter = () => {
   return (
     <>
+
       <BrowserRouter>
         <NavBar />
         <Routes>
