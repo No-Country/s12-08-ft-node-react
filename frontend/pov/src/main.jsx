b0bf439685fff7b21b7ea110fed3111aa14f04cd
Hernán García <hernancurr@gmail.com>
2023-11-29 12:34:25 -0300

    Reverting merge

--- frontend/pov/src/main.jsx
+++ frontend/pov/src/main.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import "./normalize.css";
 import "./index.css";
 import AppRouter from "./router/AppRouter.jsx";
-import store from "./slices/store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <AppRouter />
   </React.StrictMode>
 );
-
--- frontend/pov/tailwind.config.js
+++ frontend/pov/tailwind.config.js
@@ -1,17 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-      fontFamily: {
-        lexend: ["Lexend"],
-     },
-     backgroundColor: {
-        'custom-color-1': '#232322',
-        'custom-color-2': '#A5A5A5',
-      },
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light"],
-  },
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
 };
