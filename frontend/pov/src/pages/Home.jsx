28dbef94f3ad124d4adcfb97e79bb264a7fdc2d4
Rolando Duarte <rolandoduarte83@gmail.com>
2023-11-29 11:22:30 -0300

    frontend-subscripciones-29-11-23

--- frontend/pov/src/pages/Home.jsx
+++ frontend/pov/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Cards } from "../components/Cards";
+import ContainerSubscriptions from "../components/Subscription/ContainerSubscriptions";
 
 export const Home = () => {
   return (
     <>
       <h1>Este es el Home-Page</h1>
       <Cards />
+      <ContainerSubscriptions />
     </>
   );
 };
--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import  ContainerSubscriptions  from '../components/Subscription/ContainerSubscriptions';
 import '../index.css';
 
 const AppRouter = () => {
-
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/containerSubscriptions" element={<ContainerSubscriptions />} />
         </Routes>
       </BrowserRouter>
     </>
