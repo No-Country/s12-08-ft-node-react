da196e28c0f16682daead33b42d5b010cf048776
Mirge <mirgeserrano@gmail.com>
2023-11-29 21:06:33 -0400

    front - modf css - 29/11/2023

--- frontend/pov/src/pages/regiterlogin/regiterlogin.jsx
+++ frontend/pov/src/pages/regiterlogin/regiterlogin.jsx
@@ -89,139 +89,231 @@ export const Registerlogin = () => {
       }
     };
 
-  // Renderizado del componente del formulario de registro
-  // return (
-  //   <main className=" font-Lexend font-sans flex min-h-screen justify-center items-center font-Lexend ">
-  //     <div class="flex items-center justify-center h-screen">
-  //       <div class="bg-gray-200 p-6 rounded-lg">
-  //        
-  //       </div>
-  //       <div className=" flex justify-end">
-  //         <button className="m-2 p-2">
-  //           <Google />
-  //         </button>
-  //         <button className="m-2 p-2">
-  //           <Facebook />
-  //         </button>
-  //         <button className=" m-2 p-2">
-  //           <Apple />
-  //         </button>
-  //         {location.pathname === "/register" ? (
-  //           <div>
-  //             <p className=" mt-2 mb-[36px] text-[12px]">
-  //               ¿Ya tienes una cuenta? <strong>Inicia sesión</strong>
-  //             </p>
-  //           </div>
-  //         ) : (
-  //           <div>
-  //             <p className=" mt-2 mb-[36px] text-[12px]">
-  //               ¿No tienes cuenta todavía?
-  //               <strong>Regístrate</strong>
-  //             </p>
-  //           </div>
-  //         )}
-  //       </div>
-  //     </div>
-  //   </main>
-  // );
+ 
   return (
+    // <div className="hero min-h-screen bg-base-200">
+    //   <div className="hero-content flex-col lg:flex-row-reverse">
+    //     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    //       {location.pathname === "/register" ? (
+    //         <div>
+    //           <h4 className="text-5xl font-">Crea tu cuenta</h4>
+    //           <p className="py-6">
+    //             Regístrate y disfruta de tus famosos favoritos
+    //           </p>
+    //         </div>
+    //       ) : null}
 
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    //       <form className="card-body" onSubmit={handleSignUp}>
+    //         {location.pathname === "/register" ? (
+    //           <div className="form-control">
+    //             {/* Input de Usuario */}
+    //             <input
+    //               className="input input-bordered"
+    //               type="text"
+    //               name="user"
+    //               value={user}
+    //               placeholder="Usuario"
+    //               onChange={onInputChange}
+    //             />
+    //             {!!userValid && formSubmited && (
+    //               <div>
+    //                 <span style={{ color: "red" }}>{userValid}</span>
+    //               </div>
+    //             )}
+
+    //             {/* Input de Nombre y Apellido */}
+    //             <input
+    //               className="input input-bordered"
+    //               type="text"
+    //               name="username"
+    //               placeholder="Nombre y apellido"
+    //               value={username}
+    //               onChange={onInputChange}
+    //             />
+    //             {!!usernameValid && formSubmited && (
+    //               <div>
+    //                 <span style={{ color: "red" }}>{usernameValid}</span>
+    //               </div>
+    //             )}
+
+    //           </div>
+    //         ) : (
+    //           <h4 className="text-5xl font-">Ingresar</h4>
+    //         )}
+
+    //         <div className="form-control">
+    //           {/* Input de Correo Electrónico */}
+    //           <input
+    //             className="input input-bordered"
+    //             type="email"
+    //             name="email"
+    //             placeholder="Correo electrónico"
+    //             value={email}
+    //             onChange={onInputChange}
+    //           />
+    //           {!!emailValid && formSubmited && (
+    //             <div>
+    //               <span style={{ color: "red" }}>{emailValid}</span>
+    //             </div>
+    //           )}
+
+    //           {/* Input de Contraseña */}
+    //           <input
+    //             className="input input-bordered"
+    //             type="password"
+    //             name="password"
+    //             placeholder="Contraseña"
+    //             value={password}
+    //             onChange={onInputChange}
+    //           />
+    //           {!!passwordValid && formSubmited && (
+    //             <div>
+    //               <span style={{ color: "red" }}>{passwordValid}</span>
+    //             </div>
+    //           )}
+    //         </div>
+    //         {/* Botón de Envío */}
+    //         <div className="form-control mt-6">
+    //           <button
+    //             className="btn sm:btn-sm md:btn-md lg:btn-lg"
+    //             type="submit"
+    //             //disabled={!isFormValid}
+    //           >
+    //             Continuar
+    //           </button>
+    //         </div>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <main className="flex items-center justify-center min-h-screen min-w-screen">
+      <div className="bg-[#ffffff] p-6 rounded-lg w-full">
+        <form onSubmit={handleSignUp}>
           {location.pathname === "/register" ? (
             <div>
-              <h4 className="text-5xl font-">Crea tu cuenta</h4>
-              <p className="py-6">
+              <h4 className=" text-5xl font-bold  md:text-[40px] text-[24px]">
+                Crea tu cuenta
+              </h4>
+              <p className=" mt-2 mb-[36px] text-[12px]">
                 Regístrate y disfruta de tus famosos favoritos
               </p>
             </div>
           ) : null}
+          {location.pathname === "/register" ? (
+            <div className=" form-control">
+              {/* Input de Usuario */}
+              <input
+                className="mb-[20px] flex w-full h-16 p-2 items-center gap-2 flex-shrink-0 rounded-lg bg-opacity-30  bg-[#A5A5A5]"
+                type="text"
+                name="user"
+                value={user}
+                placeholder="Usuario"
+                onChange={onInputChange}
+              />
+              {!!userValid && formSubmited && (
+                <div>
+                  <span style={{ color: "red" }}>{userValid}</span>
+                </div>
+              )}
 
-          <form className="card-body" onSubmit={handleSignUp}>
-            {location.pathname === "/register" ? (
-              <div className="form-control">
-                {/* Input de Usuario */}
-                <input
-                  className="input input-bordered"
-                  type="text"
-                  name="user"
-                  value={user}
-                  placeholder="Usuario"
-                  onChange={onInputChange}
-                />
-                {!!userValid && formSubmited && (
-                  <div>
-                    <span style={{ color: "red" }}>{userValid}</span>
-                  </div>
-                )}
+              {/* Input de Nombre y Apellido */}
+              <input
+                className="mb-[20px] flex w-full h-16 p-2 items-center gap-2 flex-shrink-0 rounded-lg bg-opacity-30   bg-[#A5A5A5]"
+                type="text"
+                name="username"
+                placeholder="Nombre y apellido"
+                value={username}
+                onChange={onInputChange}
+              />
+              {!!usernameValid && formSubmited && (
+                <div>
+                  <span style={{ color: "red" }}>{usernameValid}</span>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div>
+              <h4 className="text-5xl font-bold  md:text-[40px] text-[24px]">
+                ¡Hola de nuevo!
+              </h4>
+              <p className=" mt-2 mb-[36px] text-[12px]">
+                Ingresa tus datos para continuar
+              </p>
+            </div>
+          )}
 
-                {/* Input de Nombre y Apellido */}
-                <input
-                  className="input input-bordered"
-                  type="text"
-                  name="username"
-                  placeholder="Nombre y apellido"
-                  value={username}
-                  onChange={onInputChange}
-                />
-                {!!usernameValid && formSubmited && (
-                  <div>
-                    <span style={{ color: "red" }}>{usernameValid}</span>
-                  </div>
-                )}
-
+          <div className="form-control">
+            {/* Input de Correo Electrónico */}
+            <input
+              className="mb-[20px] flex w-full h-16 p-2 items-center gap-2 flex-shrink-0 rounded-lg bg-opacity-30  bg-[#A5A5A5] "
+              type="email"
+              name="email"
+              placeholder="Correo electrónico"
+              value={email}
+              onChange={onInputChange}
+            />
+            {!!emailValid && formSubmited && (
+              <div>
+                <span style={{ color: "red" }}>{emailValid}</span>
               </div>
-            ) : (
-              <h4 className="text-5xl font-">Ingresar</h4>
             )}
 
+            {/* Input de Contraseña */}
+            <input
+              className="mb-[20px] flex w-full h-16 p-2 items-center gap-2 flex-shrink-0 rounded-lg bg-opacity-30  bg-[#A5A5A5]"
+              type="password"
+              name="password"
+              placeholder="Contraseña"
+              value={password}
+              onChange={onInputChange}
+            />
+            {!!passwordValid && formSubmited && (
+              <div>
+                <span style={{ color: "red" }}>{passwordValid}</span>
+              </div>
+            )}
+          </div>
+          {/* Botón de Envío */}
+          <div className="form-control mt-6">
+            <button
+              className="btn text-white hover:bg-gray-500 flex w-full h-14 px-10 justify-center items-center gap-4 flex-shrink-0 border rounded-md bg-[#232322] "
+              type="submit"
+              //disabled={!isFormValid}
+            >
+              Continuar
+            </button>
+          </div>
+          <div className="divider ">O continua con</div>
+        </form>
 
-            <div className="form-control">
-              {/* Input de Correo Electrónico */}
-              <input
-                className="input input-bordered"
-                type="email"
-                name="email"
-                placeholder="Correo electrónico"
-                value={email}
-                onChange={onInputChange}
-              />
-              {!!emailValid && formSubmited && (
-                <div>
-                  <span style={{ color: "red" }}>{emailValid}</span>
-                </div>
-              )}
-
-              {/* Input de Contraseña */}
-              <input
-                className="input input-bordered"
-                type="password"
-                name="password"
-                placeholder="Contraseña"
-                value={password}
-                onChange={onInputChange}
-              />
-              {!!passwordValid && formSubmited && (
-                <div>
-                  <span style={{ color: "red" }}>{passwordValid}</span>
-                </div>
-              )}
-            </div>
-            {/* Botón de Envío */}
-            <div className="form-control mt-6">
-              <button
-                className="btn sm:btn-sm md:btn-md lg:btn-lg"
-                type="submit"
-                //disabled={!isFormValid}
-              >
-                Continuar
-              </button>
-            </div>
-          </form>
+        <div className="flex justify-between w-full ">
+          <button className="btn  w-[80px] sm:w-[200px] text-white  hover:bg-gray-500 flex border rounded-md bg-[#232322] ">
+            <Google />
+          </button>
+          <button className="btn   w-[80px] sm:w-[200px] text-white  hover:bg-gray-500 flex boder rounded-md bg-[#232322]">
+            <Facebook />
+          </button>
+          <button className="btn   w-[80px] sm:w-[200px] text-white  hover:bg-gray-500 flex boder rounded-md   bg-[#232322]">
+            <Apple />
+          </button>
         </div>
+        {location.pathname === "/register" ? (
+          <div className="flex items-center justify-center">
+            <p className="mt-2 mb-4 text-sm ">
+              ¿Ya tienes una cuenta? <strong>Inicia sesión</strong>
+            </p>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center">
+            <p className="mt-2 mb-4 text-sm items-center">
+              ¿No tienes cuenta todavía?
+              <strong>Regístrate</strong>
+            </p>
+          </div>
+        )}
       </div>
-    </div>
+    </main>
   );
 };
 
