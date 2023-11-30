f6a94789e98c3d27b5d903705d71359f8b48f021
Emanuel Valente <emavalente.dev@gmail.com>
2023-11-30 12:49:05 -0300

    front - Cambio principal: Quito Navbar de Home - 30/11/23

--- frontend/pov/src/pages/Home.jsx
+++ frontend/pov/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import ContainerSubscriptions from "../components/Subscription/ContainerSubscriptions";
 import { History } from "../components/Histories/History";
-import NavBar from "../components/NavBar/NavBar";
 
 export const Home = () => {
   return (
     <>
       <section className="flex flex-col pt-28 px-5">
-        <NavBar />
         <History />
         <ContainerSubscriptions />
       </section>
