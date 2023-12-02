1163384c80e53d1cc0b91a98f54d456cf6d79c41
montoyitadevelp <simon1018226487@gmail.com>
2023-12-02 17:07:00 -0500

    add: Public + Private routes

--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -1,20 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { ChatsUsers } from "../pages/Chats/ChatsUsers";
-import Profile from "../pages/Profile";
-import Registerlogin from "../pages/regiterlogin/regiterlogin";
-import "../index.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { ChatsUsers } from '../pages/Chats/ChatsUsers';
+import Profile from '../pages/Profile';
+import '../index.css';
+import { LoginForm } from '../pages/Login/LoginForm';
+import { RegisterForm } from '../pages/Register/RegisterForm';
+import { Layout } from './Layout/Layout';
+import { RequireAuth } from '../slices/auth/requireAuth';
 
 const AppRouter = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Registerlogin />} />
-          <Route path="/register" element={<Registerlogin />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/chats" element={<ChatsUsers />} />
+          <Route path="/" element={<Layout />}>
+            {/* Public routes */}
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+
+            {/* Private routes */}
+            <Route element={<RequireAuth />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/chats" element={<ChatsUsers />} />
+            </Route>
+            <Route path="*" element={<h1>404, ups esta página no existe</h1>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
