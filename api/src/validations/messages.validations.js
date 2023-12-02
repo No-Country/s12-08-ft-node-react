55ecfad726f9e31cdb98fffb03728752a64ff100
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-12-01 21:57:29 -0300

    Fix validations

--- api/src/validations/messages.validations.js
+++ api/src/validations/messages.validations.js
@@ -7,6 +7,23 @@ const createMessageValidation = Joi.object({
     image: Joi.string().allow(null).when('content', { is: 'image', then: Joi.required() }),
     video: Joi.string().allow(null).when('content', { is: 'video', then: Joi.required() }),
     gif: Joi.string().allow(null).when('content', { is: 'gif', then: Joi.required() }),
-}).xor('image', 'video', 'gif');
+}).custom((value, helpers) => {
+    const { content, image, video, gif } = value;
+    console.log(image !== undefined)
+    const mediaCount = [image, video, gif].filter(media => media !== null && media !== undefined).length;
+
+    if (content === 'text') {
+        if (mediaCount > 0) {
+            return helpers.error('any.invalid');
+        }
+    } else {
+        console.log(mediaCount)
+        if (mediaCount !== 1) {
+            return helpers.error('any.invalid');
+        }
+    }
+
+    return value;
+});
 
 module.exports = {createMessageValidation} 
