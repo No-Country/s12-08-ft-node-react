1b286bad5fde076a623623dc83d059986763e53b
montoyitadevelp <simon1018226487@gmail.com>
2023-12-03 21:03:17 -0500

    fix: Improve requireAuth

--- frontend/pov/src/slices/auth/requireAuth.jsx
+++ frontend/pov/src/slices/auth/requireAuth.jsx
@@ -1,13 +1,9 @@
-import { useLocation, Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useLocation, Navigate, Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export const RequireAuth = () => {
-  const { token } = useSelector((state) => state.login);
-  const location = useLocation();
-  
-  return token ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
-};
+    const location = useLocation()
+    const { token } = useSelector((state) => state.login)
+
+    return token ? <Outlet /> : <Navigate to="/login" replace={{ from: location}} />
+}
