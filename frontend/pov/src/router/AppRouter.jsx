cad1c7d04c454c25b2b8f9a2d1b056b961a78ef7
Emanuel Valente <emavalente.dev@gmail.com>
2023-12-04 13:46:22 -0300

    front - Navbar user and no user function added - 04/12/23

--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from '../pages/Home';
-import { ChatsUsers } from '../pages/Chats/ChatsUsers';
-import Profile from '../pages/Profile';
-import '../index.css';
-import { LoginForm } from '../pages/Login/LoginForm';
-import { RegisterForm } from '../pages/Register/RegisterForm';
-import { Landing } from '../pages/Landing/Landing';
-import { RequireAuth } from '../slices/auth/RequireAuth';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { ChatsUsers } from "../pages/Chats/ChatsUsers";
+import Profile from "../pages/Profile";
+import "../index.css";
+import { LoginForm } from "../pages/Login/LoginForm";
+import { RegisterForm } from "../pages/Register/RegisterForm";
+import { Landing } from "../pages/Landing/Landing";
+import { RequireAuth } from "../slices/auth/RequireAuth";
 
 const AppRouter = () => {
   return (
@@ -14,18 +14,17 @@ const AppRouter = () => {
       <BrowserRouter>
         <Routes>
           {/* Public routes */}
-            <Route path="/" element={<Landing />} />
-            <Route path="login" element={<LoginForm />} />
-            <Route path="register" element={<RegisterForm />} />
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<LoginForm />} />
+          <Route path="register" element={<RegisterForm />} />
 
-            {/* Private routes */}
-            <Route element={<RequireAuth />}>
-              <Route path="home" element={<Home />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="chats" element={<ChatsUsers />} />
-            </Route>
-            <Route path="*" element={<h1>404, ups esta página no existe</h1>} />
-          
+          {/* Private routes */}
+          <Route element={<RequireAuth />}>
+            <Route path="home" element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="chats" element={<ChatsUsers />} />
+          </Route>
+          <Route path="*" element={<h1>404, ups esta página no existe</h1>} />
         </Routes>
       </BrowserRouter>
     </>
