5a215ffd80e7d6cc3350d6ea1ef86a71c08dcf5b
montoyitadevelp <simon1018226487@gmail.com>
2023-12-04 15:27:05 -0500

    fix: Improve Toast for show logout

--- frontend/pov/src/components/NavBar/Menu.jsx
+++ frontend/pov/src/components/NavBar/Menu.jsx
@@ -1,43 +1,68 @@
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import avatar from "../../assets/avatars/user.webp";
-import { useDispatch } from "react-redux";
-import { logout } from "../../slices/login.slice";
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import avatar from '../../assets/avatars/user.webp';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../slices/login.slice';
+import { useCallback, useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Menu = () => {
+  const { message } = useSelector((state) => state.login);
+  const [showAlert, setShowAlert] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const Logout = () => {
-    console.log("Cerrando Sesión BYE!");
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
-    localStorage.removeItem("duration");
+  console.log(message);
+  const Logout = useCallback(() => {
     dispatch(logout());
-    navigate("/");
-  };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (message > '') {
+      setShowAlert(true);
+      toast.loading(message);
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    }
+  }, [message, showAlert, navigate]);
 
   return (
-    <div className="dropdown dropdown-end">
-      {/* USER BUTTON */}
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-[44px] rounded-full">
-          <img alt="User Avatar" src={avatar} />
-        </div>
-      </label>
-      {/* USER MENU */}
-      <ul
-        tabIndex={0}
-        className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content rounded-box w-52 bg-white"
-      >
-        <li>
-          <Link to="/profile" className="justify-between">
-            Ver Perfil
-          </Link>
-          <button onClick={Logout}>Cerrar Sesion</button>
-        </li>
-      </ul>
-    </div>
+    <>
+      {showAlert && (
+        <>
+          <div>
+            <Toaster position="top-center" reverseOrder={false} /> /
+          </div>
+        </>
+      )}
+      <div className="dropdown dropdown-end">
+        {/* USER BUTTON */}
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-[44px] rounded-full">
+            <img alt="User Avatar" src={avatar} />
+          </div>
+        </label>
+        {/* USER MENU */}
+        <ul
+          tabIndex={0}
+          className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content rounded-box w-52 bg-white"
+        >
+          <li>
+            <Link to="/profile" className="justify-between">
+              Ver Perfil
+            </Link>
+            <button
+              onClick={() => {
+                Logout();
+              }}
+            >
+              Cerrar Sesion
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
