ce431f4d35214dfac2f2bcca11c5ada09882196e
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-12-05 19:49:06 -0300

    get user + chat + messages + comments

--- api/src/controllers/chats.controller.js
+++ api/src/controllers/chats.controller.js
@@ -1,6 +1,9 @@
 const Chat = require('../database/mongo/chats.model.js');
+const { User } = require('../db.js');
 const {editValidation} = require('../validations/chats.validations.js');
 const { cloudinary } = require("../config/cloudinary/index.js");
+const NotFound = require('../errorClasses/NotFound.js');
+const BadRequest = require('../errorClasses/BadRequest.js');
 
 class ChatController {
 
@@ -26,7 +29,33 @@ class ChatController {
         ...value
       }, {new: true})
 
-      res.status(201).json({ message: "Chat editado exitosamente", newChat: newChat });
+      return res.status(201).json({ message: "Chat editado exitosamente", newChat: newChat });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getChatWithMessages(req, res, next){
+    try {
+      const id = req.params.id
+
+      if(!id){
+        throw new BadRequest('El id es necesario')
+      }
+
+      const user = await User.findByPk( id , {attributes: ['id', 'name', 'username', 'profile_picture']})
+
+      if(!user){
+        throw new NotFound('El usuario no existe')
+      }
+
+      const chat = await Chat.findById(id).populate({path: 'messages' , populate: { path: 'comments' }})
+
+      if(!chat){
+        throw new NotFound('No se encontro el chat')
+      }
+
+      return res.status(200).json({ user: user , chat: chat });
     } catch (error) {
       next(error);
     }
