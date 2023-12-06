53a108db55ff81a76645b0ff9cb1b0e37280435f
Hernán García <hernancurr@gmail.com>
2023-12-06 17:10:45 -0300

    Backend - Edited socket accesibility and Added emit in Message creation

--- api/src/controllers/messages.controller.js
+++ api/src/controllers/messages.controller.js
@@ -4,29 +4,31 @@ const BadRequest = require("../errorClasses/BadRequest.js");
 const NotFound = require("../errorClasses/NotFound.js");
 const Unauthorized = require("../errorClasses/Unauthorized.js");
 const {
-  createMessageValidation, deleteMessageValidation,editMessageValidation
+  createMessageValidation,
+  deleteMessageValidation,
+  editMessageValidation,
 } = require("../validations/messages.validations.js");
 const { cloudinary } = require("../config/cloudinary/index.js");
+const { getIO } = require("../socket.js");
 
 class MessageController {
   static async create(req, res, next) {
-    
-    req.body.user_id = req.user_id
+    req.body.user_id = req.user_id;
     try {
       const { error, value } = createMessageValidation.validate(req.body);
       if (error) {
         throw new BadRequest(error.details[0].message);
       }
 
-      const { user_id , text, content } = value;
+      const { user_id, text, content } = value;
 
-      if(content !== 'text'){
-        const valueCont = value[content]
+      if (content !== "text") {
+        const valueCont = value[content];
         const uploadResponse = await cloudinary.uploader.upload(valueCont, {
           resource_type: "auto",
           folder: "pov",
         });
-      
+
         value[content] = uploadResponse.secure_url;
       }
 
@@ -43,6 +45,9 @@ class MessageController {
       chat.messages.push(message._id);
       chat.save();
 
+      const io = getIO();
+      io.emit("new-message", message);
+
       res
         .status(201)
         .json({ message: "Mensaje creado exitosamente", newMessage: message });
@@ -52,13 +57,13 @@ class MessageController {
     }
   }
 
-  static async editMessage(req, res, next){
+  static async editMessage(req, res, next) {
     try {
-      const user_id = req.user_id
-      const message_id = req.params.id
+      const user_id = req.user_id;
+      const message_id = req.params.id;
 
-      if(!message_id){
-        throw new BadRequest("El id del mensaje es necesario")
+      if (!message_id) {
+        throw new BadRequest("El id del mensaje es necesario");
       }
 
       const { error, value } = editMessageValidation.validate(req.body);
@@ -66,18 +71,18 @@ class MessageController {
         throw new BadRequest(error.details[0].message);
       }
 
-      const chat = await Chat.findById( user_id )
+      const chat = await Chat.findById(user_id);
 
-      const messageExist = chat.messages.includes( message_id )
+      const messageExist = chat.messages.includes(message_id);
 
-      if(!messageExist){
-        throw new NotFound("El mensaje no existe en el chat")
+      if (!messageExist) {
+        throw new NotFound("El mensaje no existe en el chat");
       }
 
       const { text, content } = value;
 
-      if(content !== 'text'){
-        const valueCont = value[content]
+      if (content !== "text") {
+        const valueCont = value[content];
         const uploadResponse = await cloudinary.uploader.upload(valueCont, {
           resource_type: "auto",
           folder: "pov",
@@ -86,46 +91,53 @@ class MessageController {
         value[content] = uploadResponse.secure_url;
       }
 
-      const newMessage = await Messages.findByIdAndUpdate(message_id ,{
-        content,
-        text,
-        image: value.image,
-        video: value.video,
-        gif: value.gif,
-      }, {new: true})
+      const newMessage = await Messages.findByIdAndUpdate(
+        message_id,
+        {
+          content,
+          text,
+          image: value.image,
+          video: value.video,
+          gif: value.gif,
+        },
+        { new: true }
+      );
 
-
-    res.status(201).json({ message: "Mensaje editado exitosamente", newMessage: newMessage });
+      res.status(201).json({
+        message: "Mensaje editado exitosamente",
+        newMessage: newMessage,
+      });
     } catch (error) {
       next(error);
     }
   }
-  
+
   static async delete(req, res, next) {
-    req.body.user_id = req.user_id
+    req.body.user_id = req.user_id;
     try {
       const { error, value } = deleteMessageValidation.validate(req.body);
       if (error) {
         throw new BadRequest(error.details[0].message);
       }
 
-      const { user_id , chat_id, message_id } = value;
+      const { user_id, chat_id, message_id } = value;
 
       if (user_id === chat_id) {
-        const message = await Messages.findByIdAndDelete({_id:message_id});
+        const message = await Messages.findByIdAndDelete({ _id: message_id });
         const chat = await Chat.findById(user_id);
         chat.messages = chat.messages.flatMap((item) => {
           if (item.toString() === message_id) {
-            return []
+            return [];
           }
-          return item
-        })
-        chat.markModified("messages")
-        chat.save()
-        res
-        .status(201)
-        .json({ message: "Mensaje borrado exitosamente", deletedMessage: message });
-      }else {
+          return item;
+        });
+        chat.markModified("messages");
+        chat.save();
+        res.status(201).json({
+          message: "Mensaje borrado exitosamente",
+          deletedMessage: message,
+        });
+      } else {
         throw new Unauthorized("No es el propietario del mensaje");
       }
     } catch (err) {
@@ -137,37 +149,43 @@ class MessageController {
     try {
       const { messageId } = req.params;
       const { user_id, reaction } = req.body;
-  
+
       const message = await Messages.findById(messageId);
-  
+
       if (!message) {
-        throw new BadRequest('Mensaje no encontrado');
+        throw new BadRequest("Mensaje no encontrado");
       }
-  
+
       if (!message.reactions) {
         message.reactions = {
-          users_who_reacted: []
+          users_who_reacted: [],
         };
       }
-  
+
       const user_reaction = {
-        "user_id": user_id,
-        "reaction": reaction
+        user_id: user_id,
+        reaction: reaction,
       };
-  
-      const isUserReaction = message.reactions.users_who_reacted.find(user => user.user_id === user_id);
+
+      const isUserReaction = message.reactions.users_who_reacted.find(
+        (user) => user.user_id === user_id
+      );
 
       if (isUserReaction && isUserReaction.reaction === reaction) {
         // Si la reacción es la misma que la anterior, se elimina
-        const indexToRemove = message.reactions.users_who_reacted.findIndex(user => user.user_id === user_id);
+        const indexToRemove = message.reactions.users_who_reacted.findIndex(
+          (user) => user.user_id === user_id
+        );
         message.reactions.users_who_reacted.splice(indexToRemove, 1);
         message.reactions[isUserReaction.reaction]--;
       } else if (isUserReaction && isUserReaction.reaction !== reaction) {
         // Si la reacción cambia, se elimina la anterior y se agrega la nueva
-        const indexToRemove = message.reactions.users_who_reacted.findIndex(user => user.user_id === user_id);
+        const indexToRemove = message.reactions.users_who_reacted.findIndex(
+          (user) => user.user_id === user_id
+        );
         message.reactions.users_who_reacted.splice(indexToRemove, 1);
         message.reactions[isUserReaction.reaction]--;
-      //Acá despues de eliminar la reación anterior, agrego la nueva!
+        //Acá despues de eliminar la reación anterior, agrego la nueva!
         message.reactions.users_who_reacted.push(user_reaction);
         message.reactions[reaction] = (message.reactions[reaction] || 0) + 1;
       } else {
@@ -175,16 +193,17 @@ class MessageController {
         message.reactions.users_who_reacted.push(user_reaction);
         message.reactions[reaction] = (message.reactions[reaction] || 0) + 1;
       }
-  
+
       await message.save();
-  
-      res.status(200).json({ message: 'Reacción actualizada exitosamente', updatedMessage: message });
+
+      res.status(200).json({
+        message: "Reacción actualizada exitosamente",
+        updatedMessage: message,
+      });
     } catch (error) {
       next(error);
     }
   }
-  
-  
-};
+}
 
 module.exports = { MessageController };
