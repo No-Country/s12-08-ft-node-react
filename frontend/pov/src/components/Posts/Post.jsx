b4231b2c4880f1d2863a262b5e84d91c50fafc13
Emanuel Valente <emavalente.dev@gmail.com>
2023-12-11 17:53:47 -0300

    front - Chat Conection with backend to render - 11/12/2023

--- frontend/pov/src/components/Posts/Post.jsx
+++ frontend/pov/src/components/Posts/Post.jsx
@@ -4,19 +4,20 @@ import ThreadUnion from "../Svg/ThreadUnion";
 import Response from "./Response";
 
 const Post = ({ post, userName, userAvatar }) => {
-  const { message, imageAdded, responses } = post;
+  const { text, comments, reactions } = post;
 
   return (
     <article className="flex flex-col gap-2 px-2 py-4 rounded-lg">
-      {/* Post */}
+      {/* Imagen del Post adjunta */}
       <div className="py-1 px-4 flex flex-col justify-center items-center bg-[#C3C3BF] rounded-lg">
-        {imageAdded && (
+        {/* {imageAdded && (
           <img
             src={imageAdded}
             alt="imagen adjunta al post"
             className="mb-2 rounded-lg overflow-hidden"
           />
-        )}
+        )} */}
+        {/* Texto del Post */}
         <div className="w-full flex gap-2 items-center">
           <img
             src={userAvatar}
@@ -25,28 +26,31 @@ const Post = ({ post, userName, userAvatar }) => {
           />
           <p className="w-full p-2 text-[12px]">
             <span className="font-black">{userName}: </span>
-            {message}
+            {text}
           </p>
         </div>
       </div>
 
       {/* Link a Respuestas del post */}
-      <div className="flex gap-1 pl-[28px]">
-        <ThreadUnion />
-        <Link
-          to=""
-          className="w-full p-2 flex gap-2 items-center bg-[#C3C3BF] rounded-lg"
-        >
-          <Response response={responses[0]} />
-        </Link>
-      </div>
+
+      {comments.length > 0 && (
+        <div className="flex gap-1 pl-[28px]">
+          <ThreadUnion />
+          <Link
+            to=""
+            className="w-full p-2 flex gap-2 items-center bg-[#C3C3BF] rounded-lg"
+          >
+            <Response responses={comments[0]} />
+          </Link>
+        </div>
+      )}
 
       {/* Reacciones con Emojis */}
       <div className="flex gap-1">
-        {post &&
-          post?.reactions.map((reaction) => (
+        {/* {post &&
+          reactions.map((reaction) => (
             <Reaction key={reaction.id} reaction={reaction} />
-          ))}
+          ))} */}
       </div>
     </article>
   );
