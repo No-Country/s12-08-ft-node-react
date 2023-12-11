e3508506d5737cddeb5af7379206debaffac3d26
david s <santisonzini1234@gmail.com>
2023-12-11 20:33:20 -0300

    stripe create subscription

--- api/src/routes/payments.js
+++ api/src/routes/payments.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const paymentsRouter = express.Router();
 const { PaymentController } = require("../controllers/payments.controller");
+const { checkSession } = require("../middlewares/session/session");
 
-paymentsRouter.get("/", PaymentController.example);
+paymentsRouter.post("/subscribe/:id",checkSession, PaymentController.createSubscriptionStripe);
 
 module.exports = paymentsRouter;
