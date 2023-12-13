0b7145be2751c1e341b764cb5114de575353dee7
montoyitadevelp <simon1018226487@gmail.com>
2023-12-13 09:52:48 -0500

    fix: Config app router

--- frontend/pov/src/router/AppRouter.jsx
+++ frontend/pov/src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { LoginForm } from '../pages/Login/LoginForm';
 import { RegisterForm } from '../pages/Register/RegisterForm';
 import { RequireAuth } from '../slices/auth/RequireAuth';
 import { ChatProvider } from '../context/ChatContext';
-import Configurations from '../pages/Configurations';
+import Configurations from '../pages/Profile/Configurations';
 import ChatContainer from '../pages/Chats/ChatContainer';
 import NotFound from '../pages/NotFound/NotFound';
 import ProfileContainer from '../pages/Profile/ProfileContainer';
@@ -25,16 +25,17 @@ const AppRouter = () => {
             <Route path="login" element={<LoginForm />} />
             <Route path="register" element={<RegisterForm />} />
 
-          {/* Private routes */}
-          <Route element={<RequireAuth />}>
-            <Route path="home" element={<Home />} />
-            <Route path="config" element={<Configurations />} />
-            <Route path="chats/:id" element={<ChatContainer />} />
-            <Route path="Profile" element={<ProfileContainer />} />
-          </Route>
-          <Route path="*" element={<NotFound/>} /> 
-        </Routes>
-      </BrowserRouter>
+            {/* Private routes */}
+            <Route element={<RequireAuth />}>
+              <Route path="home" element={<Home />} />
+              <Route path="config" element={<Configurations />} />
+              <Route path="chats/:id" element={<ChatContainer />} />
+              <Route path="Profile" element={<ProfileContainer />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </ChatProvider>
     </>
   );
 };
