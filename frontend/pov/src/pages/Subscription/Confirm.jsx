17abfc7d95a14658e3bbfd0456e7b0c76be6905d
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-12-14 19:23:04 -0300

    fix

--- frontend/pov/src/pages/Subscription/Confirm.jsx
+++ frontend/pov/src/pages/Subscription/Confirm.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import { useToken } from "../../hooks/useToken"
+import { useParams } from 'react-router-dom';
+import { URL } from "../../router/routes";
 export const Confirm = () => {
+    const [user, setUser] = useState({})
     const TOKEN = JSON.parse(useToken().token)
+    const { id } = useParams();
+
+
+    useEffect(() => {
+        const getUser = async () => {
+            const response = await fetch(`${URL}/users/${id}`)
+            console.log(response)
+        }
+        getUser
+        
+    },[id])
+
+    console.log(id)
 
     
     return(
