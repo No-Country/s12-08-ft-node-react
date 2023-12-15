21dec7d33101eb501d06f7e96da8aa726329a4fd
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-12-14 22:49:10 -0300

    fix icon and post in confirm

--- frontend/pov/src/pages/Subscription/Confirm.jsx
+++ frontend/pov/src/pages/Subscription/Confirm.jsx
@@ -11,7 +11,7 @@ export const Confirm = () => {
 
     useEffect(() => {
         const getUser = async () => {
-            const response = await fetch(`${URL}/users`, {headers: {
+            const response = await fetch(`${URL}/users?profile=${id}`, {headers: {
                 Authorization: `Bearer ${TOKEN}`,
             }})
 
@@ -21,10 +21,17 @@ export const Confirm = () => {
         }
         getUser()
         
-    },[id])
+    },[TOKEN, id])
 
-    const handleConfirm = () => {
-      console.log("confirm")
+    const handleConfirm = async() => {
+      console.log(id)
+      const response = await fetch(`${URL}/payments/subscribe/${id}`, {method: "POST", headers:{
+        Authorization: `Bearer ${TOKEN}`,
+      }})
+
+      const data = await response.json()
+
+      window.location.href = data.session.url
     }
 
 
@@ -34,28 +41,31 @@ export const Confirm = () => {
     
     return(
         <>
-      <header className="w-full px-[24px] py-[10px] mt-[96px] mb-[12px] flex bg-white">
-        <div className="w-full md:max-w-[1000px] lg:mx-auto items-center ">
-          <h2 className="text-[20px]">Datos de Subscripción</h2>
-        </div>
-      </header>
-      <main className="w-full md:max-w-[1000px] lg:mx-auto px-[24px]">
-        <h3 className="flex flex-row">{beneficiary.username} <Verified /> </h3>
-        <p>{beneficiary.suscribersCount} suscriptores</p>
-        <img className="w-40 h-40 rounded-full mt-4" src={beneficiary.profile_picture ? beneficiary.profile_picture : 'https://res.cloudinary.com/dkgvoukdj/image/upload/v1702563393/pov/uaotuzpgadixhmaoxtxy.avif'}></img>
-        <p className="mt-6">Lorem ipsum dolor sit amet consectetur. Nunc lacus iaculis molestie proin placerat. Urna diam ornare diam bibendum eget. Amet at pellentesque netus ut elit. Dui pulvinar ultrices vivamus ultrices accumsan mauris at molestie. Ante a quis tempor ac tellus. Tristique purus eget vitae.</p>
-        <div className="flex space-x-48 mt-6">
-          <p>$5.00</p>
-          <p>Mensuales</p>
-        </div>
-        <div className="flex flex-col">
-          <button className="btn text-white hover:bg-gray-500 flex w-full h-14 px-10 justify-center items-center gap-4 flex-shrink-0 border rounded-md bg-[#232322]" onClick={handleConfirm}>
-            Continuar con el pago</button>
-          <button className="btn text-dark hover:bg-gray-500 flex w-full h-14 px-10 justify-center items-center gap-4 flex-shrink-0 border rounded-md bg-[#DADADA]" onClick={handleCancel}>
-            Cancelar
-          </button>
-        </div>
-      </main>
+          <header className="w-full mt-[96px] mb-[12px] flex bg-white text-center">
+            <div className="w-full md:max-w-[1000px] lg:mx-auto items-center ">
+              <h2 className="text-[20px]">Datos de Subscripción</h2>
+            </div>
+          </header>
+          <main className="w-full md:max-w-[1000px] lg:mx-auto px-[24px] text-center">
+            <div className="flex text-center items-center justify-center">
+                <p>{beneficiary.username}</p>
+                <Verified />
+            </div>
+            <p>{beneficiary.suscribersCount} suscriptores</p>
+            <img className="w-40 h-40 rounded-full mt-4 mx-auto my-auto" src={beneficiary.profile_picture ? beneficiary.profile_picture : 'https://res.cloudinary.com/dkgvoukdj/image/upload/v1702563393/pov/uaotuzpgadixhmaoxtxy.avif'}></img>
+            <p className="mt-6">Lorem ipsum dolor sit amet consectetur. Nunc lacus iaculis molestie proin placerat. Urna diam ornare diam bibendum eget. Amet at pellentesque netus ut elit. Dui pulvinar ultrices vivamus ultrices accumsan mauris at molestie. Ante a quis tempor ac tellus. Tristique purus eget vitae.</p>
+            <div className="flex space-x-48 mt-6">
+              <p>$5.00</p>
+              <p>Mensuales</p>
+            </div>
+            <div className="flex flex-col">
+              <button className="btn mt-2 text-white hover:bg-gray-500 flex w-full h-14 px-10 justify-center items-center gap-4 flex-shrink-0 border rounded-md bg-[#232322]" onClick={handleConfirm}>
+                Continuar con el pago</button>
+              <button className="btn mt-2 text-dark hover:bg-gray-500 flex w-full h-14 px-10 justify-center items-center gap-4 flex-shrink-0 border rounded-md bg-[#DADADA]" onClick={handleCancel}>
+                Cancelar
+              </button>
+            </div>
+          </main>
         </>
     )
 }
